8110ba119ed39c9b31d266ba69b4e7a47f588b70
AcclaimedAP <alexander.w.pressfelt@gmail.com>
2023-03-20 12:05:58 +0100

    added routes

---
+++ backend/routes/api.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+var usersRouter = require('./users');
+var productsRouter = require('./products');
+var ordersRouter = require('./orders');
+router.use('/users', usersRouter);
+router.use('/products', productsRouter);
+router.use('/orders', ordersRouter);
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('Api calls');
+});
+
+module.exports = router;
