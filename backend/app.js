823bd8dc35bf594483bb6762b4d7bc62da74a9b8
AcclaimedAP <alexander.w.pressfelt@gmail.com>
2023-03-20 11:19:25 +0100

    added mongodb connection to database

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,21 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+const MongoClient = require('mongodb').MongoClient;
+
 var app = express();
+
+
+MongoClient.connect('mongodb://127.0.0.1:27017', {
+    useUnifiedTopology: true
+})
+    .then(client => {
+        console.log("Connected to Database");
+        const db = client.db('alexander-pressfelt');
+
+        app.locals.db = db;
+})
+
 
 app.use(logger('dev'));
 app.use(express.json());
